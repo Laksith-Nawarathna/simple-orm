f9b7582f7dd345b778a0148380f47bed05ce5d97
Eranga Bandara <erangabandara94@gmail.com>
2022-11-26 22:43:22 +0530

    Implement initialize method

--- src/main/java/lk/ijse/dep9/orm/InitializeDB.java
+++ src/main/java/lk/ijse/dep9/orm/InitializeDB.java
@@ -1,9 +1,16 @@
 package lk.ijse.dep9.orm;
 
+import lk.ijse.dep9.orm.annotation.Table;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class InitializeDB {
 
@@ -29,6 +36,34 @@ public class InitializeDB {
             connection = DriverManager.getConnection(url, username, password);
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        }
+
+        ArrayList<String> classNames = new ArrayList<>();
+
+        for (String packageToScan : packagesToScan) {
+            var packageName = packageToScan;
+            packageToScan = packageToScan.replaceAll("[.]", "/");
+            URL packageUrl = InitializeDB.class.getResource("/" + packageToScan);
+            try {
+                File file = new File(packageUrl.toURI());
+                classNames.addAll(Arrays.asList(file.list()).stream()
+                        .map(name -> packageName + "." + name.replace(".class",""))
+                        .collect(Collectors.toList()));
+            } catch (URISyntaxException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        for (String className : classNames) {
+            try {
+                Class<?> loadedClass = Class.forName(className);
+                Table tableAnnotation = loadedClass.getDeclaredAnnotation(Table.class);
+                if(tableAnnotation != null){
+                    createTable(loadedClass, connection);
+                }
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
